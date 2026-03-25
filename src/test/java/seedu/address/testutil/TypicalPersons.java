05155f50dc10fd557135ea5995908d4f1a4cc386
Isha <ishasovasaria@gmail.com>
2026-03-25 19:36:36 +0800

    Fix TypicalPersons.java as per new validation rules for various fields of Person class

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -1,12 +1,22 @@
 package seedu.address.testutil;
 
+// import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+// import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TELE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TELE_BOB;
+// import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+// import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Progress;
 
 /**
  * A utility class containing a list of {@code Person} objects to be used in tests.
@@ -17,58 +27,85 @@ public class TypicalPersons {
             .withCourseId("CS2103T")
             .withStudentId("A1234567G")
             .withTGroup("T08")
-            .withEmail("alice@example.com")
+            .withEmail("alice@u.nus.edu")
             .withTele("alice_pauline")
-            .withProgress(Progress.ON_TRACK)
             .build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withCourseId("CS2101")
             .withStudentId("A1234567F")
             .withTGroup("T09")
-            .withEmail("johnd@example.com")
+            .withEmail("johnd@u.nus.edu")
             .withTele("johndoe")
-            .withProgress(Progress.AT_RISK)
             .build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz")
             .withTele("carl_kurz")
-            .withEmail("heinz@example.com")
+            .withEmail("heinz@u.nus.edu")
             .withCourseId("CS1101S")
             .withStudentId("A1234567H")
             .withTGroup("T01")
-            .withProgress(Progress.NEEDS_ATTENTION)
             .build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier")
             .withTele("daniel_meier")
-            .withEmail("cornelia@example.com")
+            .withEmail("cornelia@u.nus.edu")
             .withCourseId("HSS1000")
             .withStudentId("A1234567I")
             .withTGroup("T02")
-            .withProgress(Progress.ON_TRACK)
             .build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer")
             .withTele("elle_meyer")
-            .withEmail("werner@example.com")
+            .withEmail("werner@u.nus.edu")
             .withCourseId("GEN2061X")
             .withStudentId("A1234567J")
             .withTGroup("T03")
-            .withProgress(Progress.ON_TRACK)
             .build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz")
             .withTele("fiona_kunz")
-            .withEmail("lydia@example.com")
+            .withEmail("lydia@u.nus.edu")
             .withCourseId("CS2103T")
             .withStudentId("A1234567K")
             .withTGroup("T04")
-            .withProgress(Progress.AT_RISK)
             .build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best")
             .withTele("george_best")
-            .withEmail("anna@example.com")
+            .withEmail("anna@u.nus.edu")
             .withCourseId("CS2101")
             .withStudentId("A1234567L")
             .withTGroup("T05")
-            .withProgress(Progress.NEEDS_ATTENTION)
             .build();
+
+    // Manually added
+    public static final Person HOON = new PersonBuilder().withName("Hoon Meier")
+            .withTele("hoon_meier")
+            .withEmail("stefan@u.nus.edu")
+            .withCourseId("CS2103T")
+            .withStudentId("A1234567M")
+            .withTGroup("T06")
+            .build();
+    public static final Person IDA = new PersonBuilder().withName("Ida Mueller")
+            .withTele("ida_mueller")
+            .withEmail("hans@u.nus.edu")
+            .withCourseId("CS2101")
+            .withStudentId("A1234567N")
+            .withTGroup("T07")
+            .build();
+
+    // Manually added - Person's details found in {@code CommandTestUtil}
+    public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY)
+            .withCourseId("CS2102")
+            .withStudentId("A1234567O")
+            .withTGroup("T10")
+            .withTele(VALID_TELE_AMY)
+            .withEmail(VALID_EMAIL_AMY)
+            .build();
+    public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB)
+            .withCourseId("CS2102")
+            .withStudentId("A1234567P")
+            .withTGroup("T11")
+            .withTele(VALID_TELE_BOB)
+            .withEmail(VALID_EMAIL_BOB)
+            .build();
+
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
     private TypicalPersons() {} // prevents instantiation
 
