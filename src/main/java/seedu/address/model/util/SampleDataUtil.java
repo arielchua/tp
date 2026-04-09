da3448307ed18e3cfea98ce895284e7875bc0adf
hyder <amirhyderfeisal@gmail.com>
2026-04-09 20:02:25 +0800

    Made it more diverse

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -27,9 +27,14 @@ public class SampleDataUtil {
             new StudentId("A1234567X"),
             new TGroup("T12"),
             new Tele("@alexyeoh"),
-            new WeekList(),
+            createWeekList(
+                new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+                new int[] {},
+                new int[] {}
+            ),
             Progress.ON_TRACK);
-        alex.addRemark(new Remark("Participates actively in class", LocalDate.now()));
+        alex.addRemark(new Remark("Participates actively in class", LocalDate.of(2026, 2, 3)));
+        alex.addRemark(new Remark("Usually well-prepared for tutorials", LocalDate.of(2026, 3, 10)));
 
         Person bernice = new Person(
             new Name("Bernice Yu"),
@@ -38,71 +43,99 @@ public class SampleDataUtil {
             new StudentId("A7654321X"),
             new TGroup("T12"),
             new Tele("@berniceyu"),
-            new WeekList(),
+            createWeekList(
+                new int[] {0, 1, 2, 4, 5, 7, 8, 9, 10},
+                new int[] {3, 6, 11},
+                new int[] {}
+            ),
             Progress.NEEDS_ATTENTION);
+            bernice.addRemark(new Remark("Attendance is inconsistent in later weeks", LocalDate.of(2026, 3, 24)));
 
         Person charlotte = new Person(
             new Name("Charlotte Oliveiro"),
-            new CourseId("CS2103T"),
+            new CourseId("CS2030S"),
             new Email("charlotte@u.nus.edu"),
             new StudentId("A1111111X"),
-            new TGroup("T12"),
+            new TGroup("T08"),
             new Tele("@charlotte"),
-            new WeekList(),
+            createWeekList(
+                new int[] {},
+                new int[] {0, 1, 2, 3, 4, 5},
+                new int[] {6}
+            ),
             Progress.NOT_SET);
-        charlotte.addRemark(new Remark("Has not attended any tutorial sessions", LocalDate.now()));
+        charlotte.addRemark(new Remark("Has missed multiple tutorial sessions", LocalDate.of(2026, 3, 1)));
 
         Person david = new Person(
             new Name("David Li"),
-            new CourseId("CS2103T"),
+            new CourseId("CS2030S"),
             new Email("lidavid@u.nus.edu"),
             new StudentId("A2222222X"),
-            new TGroup("T12"),
+            new TGroup("T08"),
             new Tele("@lidavid"),
-            new WeekList(),
+            createWeekList(
+                new int[] {0, 1, 2, 3, 5, 6, 7, 9, 10, 11},
+                new int[] {4, 8},
+                new int[] {}
+            ),
             Progress.NOT_SET);
 
         Person irfan = new Person(
             new Name("Irfan Ibrahim"),
-            new CourseId("CS2103T"),
+            new CourseId("CS2040S"),
             new Email("irfan@u.nus.edu"),
             new StudentId("A3333333X"),
-            new TGroup("T12"),
+            new TGroup("T05"),
             new Tele("@irfan"),
-            new WeekList(),
+            createWeekList(
+                new int[] {0, 2, 3, 5, 6},
+                new int[] {1, 4, 7, 8},
+                new int[] {9}
+            ),
             Progress.AT_RISK);
-        irfan.addRemark(new Remark("Needs to improve participation", LocalDate.now()));
+        irfan.addRemark(new Remark("Needs to improve participation", LocalDate.of(2026, 2, 24)));
+        irfan.addRemark(new Remark("Seems to be struggling with weekly material", LocalDate.of(2026, 3, 1)));
 
         Person roy = new Person(
             new Name("Roy Balakrishnan"),
-            new CourseId("CS2103T"),
+            new CourseId("CS2040S"),
             new Email("royb@u.nus.edu"),
             new StudentId("A4444444X"),
-            new TGroup("T12"),
+            new TGroup("T05"),
             new Tele("@royb"),
-            new WeekList(),
+            createWeekList(
+                new int[] {0, 1, 3, 4, 5, 6, 8, 9, 11},
+                new int[] {2, 7, 10},
+                new int[] {}
+            ),
             Progress.NEEDS_ATTENTION);
 
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new CourseId("CS2103T"), new Email("alexyeoh@u.nus.edu"),
-                new StudentId("A1234567X"), new TGroup("T12"), new Tele("@alexyeoh"),
-                    new WeekList(), Progress.ON_TRACK),
-            new Person(new Name("Bernice Yu"), new CourseId("CS2103T"), new Email("berniceyu@u.nus.edu"),
-                new StudentId("A7654321X"), new TGroup("T12"), new Tele("@berniceyu"),
-                    new WeekList(), Progress.NEEDS_ATTENTION),
-            new Person(new Name("Charlotte Oliveiro"), new CourseId("CS2103T"), new Email("charlotte@u.nus.edu"),
-                new StudentId("A1111111X"), new TGroup("T12"), new Tele("@charlotte"),
-                    new WeekList(), Progress.NOT_SET),
-            new Person(new Name("David Li"), new CourseId("CS2103T"), new Email("lidavid@u.nus.edu"),
-                new StudentId("A2222222X"), new TGroup("T12"), new Tele("@lidavid"),
-                    new WeekList(), Progress.NOT_SET),
-            new Person(new Name("Irfan Ibrahim"), new CourseId("CS2103T"), new Email("irfan@u.nus.edu"),
-                new StudentId("A3333333X"), new TGroup("T12"), new Tele("@irfan"),
-                    new WeekList(), Progress.AT_RISK),
-            new Person(new Name("Roy Balakrishnan"), new CourseId("CS2103T"), new Email("royb@u.nus.edu"),
-                new StudentId("A4444444X"), new TGroup("T12"), new Tele("@royb"),
-                    new WeekList(), Progress.NEEDS_ATTENTION)
+            alex, bernice, charlotte, david, irfan, roy
         };
+    }
+
+    /**
+     * Creates a WeekList with the given attended, absent, and cancelled week indices.
+     * Indices are 0-based:
+     * week 1 = 0, week 2 = 1, ..., week 13 = 12.
+     */
+    private static WeekList createWeekList(int[] attendedWeeks, int[] absentWeeks, int[] cancelledWeeks) {
+        WeekList weekList = new WeekList();
+
+        for (int weekIndex : attendedWeeks) {
+            weekList.markWeekAsAttended(weekIndex);
+        }
+
+        for (int weekIndex : absentWeeks) {
+            weekList.markWeekAsAbsent(weekIndex);
+        }
+
+        for (int weekIndex : cancelledWeeks) {
+            weekList.markAsCancelled(weekIndex);
+        }
+
+        return weekList;
     }
 
     public static ReadOnlyAddressBook getSampleAddressBook() {
